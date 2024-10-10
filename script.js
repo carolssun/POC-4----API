cf6652d984f2ce7f210f3171edac958abe5e115f
carolssun <carolinasuncastilho@gmail.com>
2024-10-10 20:55:03 -0300

    POC finalizada

--- script.js
+++ script.js
@@ -5,13 +5,28 @@ function getGatinho(endpoint) {
         })
         .then(urlIMG => {
             let content = document.getElementById("content");
-            content.innerHTML += `<img src="${urlIMG}" alt="Gato" style="max-width: 300px; margin: 10px;" />`;
+            content.innerHTML += `<img src="${urlIMG}" alt="Gato" style="width: 300px; margin: 10px; height: 400px" />`;
+            content.innerHTML+=`<p> Um gato fofo </p>`;
         })
         .catch(error => console.log(error))
         .finally(() => console.log("FIM"));
 }
 
 // Chama a função ao carregar a página
-getGatinho('https://cataas.com/cat'); // Gato aleatório
 getGatinho('https://cataas.com/cat/cute'); // Gato fofo
-getGatinho('https://cataas.com/cat/sleeping'); // Gato dormindo
+getGatinho2('https://cataas.com/cat/sleeping'); // Gato dormindo
+
+
+async function getGatinho2(endpoint) {
+    try {
+        let response = await fetch(endpoint);
+        let urlIMG = response.url; // Usa diretamente a URL da resposta
+        let content = document.getElementById('content2');
+        content.innerHTML += `<img src="${urlIMG}" alt="Gato" style="width: 300px; margin: 10px; height: 400px" />`;
+        content.innerHTML+=`<p> Um gato dormindo </p>`;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        console.log("fim");
+    }
+}
